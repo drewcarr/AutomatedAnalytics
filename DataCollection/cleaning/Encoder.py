e6691b3d1e45404ed998a15181097334e04265ed
Will Burnham <burnham.will2020@gmail.com>
2024-10-07 21:14:58 -0500

    adding encoding for booleans, datetime, timedelta

--- DataCollection/cleaning/Encoder.py
+++ DataCollection/cleaning/Encoder.py
@@ -5,12 +5,37 @@ class Encoder:
     def __init__(self):
         self.encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
         self.categorical_column_identifiers = ['object']
+        self.boolean_column_identifier = ['bool']
+        self.date_column_identifier = ['datetime64', 'timedelta64']
 
     def encode(self, data: pd.DataFrame) -> pd.DataFrame:
         categorical_cols = data.select_dtypes(include=self.categorical_column_identifiers).columns
+        boolean_cols = data.select_dtypes(include=self.boolean_column_identifier).columns
+        date_cols = data.select_dtypes(include=self.date_column_identifier).columns
+
         if not categorical_cols.empty:
             encoded_features = self.encoder.fit_transform(data[categorical_cols])
             encoded_df = pd.DataFrame(encoded_features, columns=self.encoder.get_feature_names_out(categorical_cols))
-            return pd.concat([data.drop(columns=categorical_cols), encoded_df], axis=1)
-        else:
-            return data
+            data = pd.concat([data.drop(columns=categorical_cols), encoded_df], axis=1)
+
+        if not boolean_cols.empty:
+            data[boolean_cols] = data[boolean_cols].astype(int)
+
+        if not date_cols.empty:
+            for col in date_cols:
+                data = _encode_date_columns(data, col)
+
+        return data
+
+def _encode_date_columns(data: pd.DataFrame, column: str) -> pd.DataFrame:
+    if data[column].dtype == 'timedelta64[ns]':
+        data[column] = data[column].apply(lambda x: x.days)
+    else:
+        data[f'{column}_year'] = data[column].dt.year
+        data[f'{column}_month'] = data[column].dt.month
+        data[f'{column}_day'] = data[column].dt.day
+        data[f'{column}_hour'] = data[column].dt.hour
+        data[f'{column}_day_of_week'] = data[column].dt.dayofweek
+        data = data.drop(columns=[column])
+    return data
+
--- tests/DataCollection/cleaning/test_Encoder.py
+++ tests/DataCollection/cleaning/test_Encoder.py
@@ -10,5 +10,51 @@ def test_encode():
     encoded_data = encoder.encode(data)
 
     expected_columns = ['feature_name_a', 'feature_name_b', 'feature_name_c']
-    assert all(col in encoded_data.columns for col in expected_columns)
-    assert encoded_data.shape[1] == len(expected_columns)
+
+    expected_values = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [1, 0, 0],
+        [0, 0, 1]
+    ]
+    assert encoded_data[expected_columns].values.tolist() == expected_values
+
+def test_encode_boolean():
+    data = pd.DataFrame({
+        'boolean_feature': [True, False, True, False]
+    })
+
+    encoder = Encoder()
+    encoded_data = encoder.encode(data)
+
+    expected_values = [1, 0, 1, 0]
+    assert encoded_data['boolean_feature'].tolist() == expected_values
+
+def test_encode_datetime():
+    data = pd.DataFrame({
+        'date_feature': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04'])
+    })
+
+    encoder = Encoder()
+    encoded_data = encoder.encode(data)
+
+    expected_data = pd.DataFrame({
+        'date_feature_year': pd.Series([2023, 2023, 2023, 2023], dtype='int32'),
+        'date_feature_month': pd.Series([1, 1, 1, 1], dtype='int32'),
+        'date_feature_day': pd.Series([1, 2, 3, 4], dtype='int32'),
+        'date_feature_hour': pd.Series([0, 0, 0, 0], dtype='int32'),
+        'date_feature_day_of_week': pd.Series([6, 0, 1, 2], dtype='int32')
+    })
+
+    pd.testing.assert_frame_equal(encoded_data, expected_data)
+
+def test_encode_timedelta():
+    data = pd.DataFrame({
+        'timedelta_feature': pd.to_timedelta(['1 days', '2 days', '3 days', '4 days'])
+    })
+
+    encoder = Encoder()
+    encoded_data = encoder.encode(data)
+
+    expected_values = [1, 2, 3, 4]
+    assert encoded_data['timedelta_feature'].tolist() == expected_values
